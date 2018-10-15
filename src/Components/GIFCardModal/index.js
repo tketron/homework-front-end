9012d339193bd67d3401bd6582d88a8cb8b9e5e4
Tyler Ketron <tketron@gmail.com>
2018-10-15 12:25:27 -0700

    improved styling on GIFCardModal

--- src/Components/GIFCardModal/index.js
+++ src/Components/GIFCardModal/index.js
@@ -6,14 +6,19 @@ export default class GIFCardModal extends Component {
   render() {
     return (
       <div className="GIFCardModal" onClick={this.props.onClick}>
-        <div className="GIFCardModal-main">
-          <div className="GIFCardModal-img-container">
-            <img src={this.props.gif.images.downsized.url} />
-          </div>
-          <div className="GIFCardModal-gif-details-container">
-            <p>
-              {this.props.gif.title} uploaded by {this.props.gif.username}
-            </p>
+        <div className="GIFCardModal-modal">
+          <div className="GIFCardModal-contents">
+            <div className="GIFCardModal-gif-container">
+              <img
+                src={this.props.gif.images.downsized.url}
+                className="GIFCardModal-gif"
+              />
+            </div>
+            <div className="GIFCardModal-gif-details-container">
+              <p>
+                {this.props.gif.title} uploaded by {this.props.gif.username}
+              </p>
+            </div>
           </div>
         </div>
       </div>
