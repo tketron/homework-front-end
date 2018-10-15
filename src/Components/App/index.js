8174dee67381f2127e1836c6aec975c6b07c82a9
Tyler Ketron <tketron@gmail.com>
2018-10-14 19:46:00 -0700

    added state and api call to GIFCardContainer

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
 
-import GIFCard from '../GIFCardContainer';
+import GIFCardContainer from '../GIFCardContainer';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true
-    };
-  }
-
   render() {
     return (
       <div className="App">
-        <GIFCard />
+        <GIFCardContainer />
       </div>
     );
   }
--- src/Components/GIFCardContainer/index.js
+++ src/Components/GIFCardContainer/index.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-export default class GIFCard extends Component {
+export default class GIFCardContainer extends Component {
+  state = {
+    loading: true,
+    gifs: []
+  };
+
+  componentDidMount() {
+    axios
+      .get('http://api.giphy.com/v1/gifs/trending', {
+        params: {
+          api_key: process.env.REACT_APP_GIPHY_KEY
+        }
+      })
+      .then(data => {
+        this.setState({ gifs: data.data.data });
+      });
+  }
+
   render() {
     return (
       <div>
         I'm the GIFCard div
-        {process.env.REACT_APP_GIPHY_KEY}
+        <ul>
+          {this.state.gifs.map(gif => (
+            <li>{gif.title}</li>
+          ))}
+        </ul>
       </div>
     );
   }
