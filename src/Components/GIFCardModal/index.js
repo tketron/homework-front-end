0530b6682fd1e31b2295623602d4823d6ac1020b
Tyler Ketron <tketron@gmail.com>
2018-10-16 11:58:19 -0700

    added loading bar to GIFCardModal component

--- src/Components/GIFCardModal/index.js
+++ src/Components/GIFCardModal/index.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import ImageLoader from 'react-load-image';
+import ReactLoading from 'react-loading';
 
 import './GIFCardModal.css';
 
 export default class GIFCardModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
+  handleImageLoad = () => {
+    console.log('loaded');
+    this.setState({ loading: false });
+  };
+
   render() {
     return (
       <div className="GIFCardModal" onClick={this.props.onClick}>
@@ -12,19 +26,31 @@ export default class GIFCardModal extends Component {
               <img
                 src={this.props.gif.images.downsized.url}
                 className="GIFCardModal-gif"
+                onLoad={this.handleImageLoad}
               />
+              {this.state.loading && (
+                <div className="GIFCardModal-loading">
+                  <ReactLoading
+                    type="bubbles"
+                    color="#000"
+                    height="20%"
+                    width="20%"
+                  />
+                </div>
+              )}
             </div>
             <div className="GIFCardModal-gif-details-container">
-              <p>
-                {this.props.gif.title} uploaded by {this.props.gif.username}
+              <p className="GIFCardModal-gif-details-text">
+                {this.props.gif.title} <em>uploaded by</em>{' '}
+                {this.props.gif.username}
               </p>
-              <p>
+              <p className="GIFCardModal-gif-details-link">
                 <a
                   href={this.props.gif.bitly_url}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  View on giphy
+                  View on Giphy
                 </a>
               </p>
             </div>
