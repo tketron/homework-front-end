1f716ee51b31b0f1db04443a79b407c6fc52adee
Tyler Ketron <tketron@gmail.com>
2018-10-16 12:05:06 -0700

    Updates

---
+++ precache-manifest.65a2d00abd7ca427c10d4254289e34a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75940af28d2a19f77e11",
+    "url": "/homework-gifeaze/static/js/runtime~main.75940af2.js"
+  },
+  {
+    "revision": "f8eed38b625f8db4d6ca",
+    "url": "/homework-gifeaze/static/js/main.f8eed38b.chunk.js"
+  },
+  {
+    "revision": "f8778f3b4a1337d17e34",
+    "url": "/homework-gifeaze/static/js/1.f8778f3b.chunk.js"
+  },
+  {
+    "revision": "f8eed38b625f8db4d6ca",
+    "url": "/homework-gifeaze/static/css/main.df51dfdd.chunk.css"
+  },
+  {
+    "revision": "3704f436f48cdbfa72945940e9df5147",
+    "url": "/homework-gifeaze/index.html"
+  }
+];
