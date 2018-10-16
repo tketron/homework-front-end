53ef2f80c5909ee58cda740229c9d7855fcaf3fa
Tyler Ketron <tketron@gmail.com>
2018-10-15 19:31:24 -0700

    added toggle and search input functions to SearchBar

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -5,10 +5,15 @@ import SearchBar from '../SearchBar';
 import GIFCardContainer from '../GIFCardContainer';
 
 export default class App extends Component {
+  handleQueryChange = (isSearch, query) => {
+    console.log(isSearch);
+    console.log(query);
+  };
+
   render() {
     return (
       <div className="App">
-        <SearchBar />
+        <SearchBar onSearchChange={this.handleQueryChange} />
         <GIFCardContainer />
       </div>
     );
--- src/Components/SearchBar/index.js
+++ src/Components/SearchBar/index.js
@@ -3,13 +3,47 @@ import './SearchBar.css';
 
 export default class SearchBar extends Component {
   state = {
-    isSearch: false,
+    isSearch: true,
     searchTerm: ''
   };
 
-  handleChange = () => {};
+  handleSearchBarChange = e => {
+    this.setState({ searchTerm: e.target.value }, () => {
+      this.props.onSearchChange(this.state.isSearch, this.state.searchTerm);
+    });
+  };
+
+  handleSearchTypeToggle = () => {
+    this.setState(state => {
+      this.props.onSearchChange(!state.isSearch);
+      return { isSearch: !state.isSearch };
+    });
+  };
 
   render() {
-    return <div>I'm the search bar</div>;
+    return (
+      <div className="SearchBar">
+        {this.state.isSearch ? (
+          <div>
+            Search:{' '}
+            <input
+              type="text"
+              value={this.state.searchTerm}
+              onChange={this.handleSearchBarChange}
+            />
+            <button onClick={this.handleSearchTypeToggle}>
+              Trending Instead?
+            </button>
+          </div>
+        ) : (
+          <div>
+            Trending
+            <button onClick={this.handleSearchTypeToggle}>
+              Search instead
+            </button>
+          </div>
+        )}
+      </div>
+    );
   }
 }
